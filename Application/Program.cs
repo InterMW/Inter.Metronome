1e993f5ee602471eaae73abc2a4914542f23ffd3
Joseph-Melberg <josephbmelbergthird@outlook.com>
2024-04-09 17:17:14 +0000

    Small improvements

--- Application/Program.cs
+++ Application/Program.cs
@@ -5,11 +5,13 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
-        await MelbergHost.CreateHost<AppRegistrator>()
+        await MelbergHost
+            .CreateHost<AppRegistrator>()
             .DevelopmentPasswordReplacement(
                     "Rabbit:ClientDeclarations:Connections:0:Password",
                     "rabbit_pass")
             .Build()
-            .StartAsync();
+            .RunAsync();
+
     }
 }
